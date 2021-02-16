4267a528ceac96a606b950f59cb7a010a1a522df
meg <sdn@netxs.online>
2021-02-16 13:22:05 +0500

    deleted old files

--- src/netxs/datetime.h
+++
@@ -1,7 +0,0 @@
-#ifndef NETXS_DATETIME_H
-#define NETXS_DATETIME_H
-
-#include "datetime/date_time.h"
-#include "datetime/quartz.h"
-
-#endif // NETXS_DATETIME_H
--- src/netxs/os.h
+++
@@ -1,9 +0,0 @@
-#ifndef NETXS_OS_H
-#define NETXS_OS_H
-
-#include "os/system.h"
-#include "os/file_system.h"
-//#include "os/mac_vendor.h"
-//#include "os/os_version.h"
-
-#endif // NETXS_OS_H
