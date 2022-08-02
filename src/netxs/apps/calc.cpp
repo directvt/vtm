8ec4b58e8d4919c105785216606852192d9b1514
Dmitry Sapozhnikov <11535558+o-sdn-o@users.noreply.github.com>
2022-08-02 21:07:57 +0500

    v0.8.0

--- src/netxs/apps/calc.cpp
+++ src/netxs/apps/calc.cpp
@@ -1,7 +1,7 @@
 // Copyright (c) NetXS Group.
 // Licensed under the MIT license.
 
-#define DESKTOPIO_VER "v0.7.6"
+#define DESKTOPIO_VER "v0.8.0"
 #define DESKTOPIO_MYNAME "Desktopio Calc " DESKTOPIO_VER
 #define DESKTOPIO_MYPATH "vtm/calc"
 #define DESKTOPIO_DEFAPP "Calc"
--- src/vtm.cpp
+++ src/vtm.cpp
@@ -1,7 +1,7 @@
 // Copyright (c) NetXS Group.
 // Licensed under the MIT license.
 
-#define DESKTOPIO_VER "v0.7.7"
+#define DESKTOPIO_VER "v0.8.0"
 #define DESKTOPIO_MYNAME "vtm " DESKTOPIO_VER
 #define DESKTOPIO_PREFIX "desktopio_"
 #define DESKTOPIO_MYPATH "vtm"
