6ca0446f00d6c2543d28bc955b23b1027920d77e
Dmitry Sapozhnikov <11535558+o-sdn-o@users.noreply.github.com>
2022-10-05 23:38:21 +0500

    #234 WIP: Fix environment variable expansion

--- src/netxs/apps.hpp
+++ src/netxs/apps.hpp
@@ -1186,11 +1186,7 @@ namespace netxs::app::shared
             {
                 auto temp = path.substr(1);
                 path = os::get_env(temp);
-                if (path.empty())
-                {
-                    log('\tfailed');
-                    return faux;
-                }
+                if (path.empty()) return faux;
                 log('\t', temp, " = ", path);
             }
             auto config_path = path.starts_with("~/") ? os::homepath() / path.substr(2)
