be4de0e8a66756e3cf796aa22d6f32dc23fcfbb7
Dmitry Sapozhnikov <11535558+o-sdn-o@users.noreply.github.com>
2022-10-06 19:23:17 +0500

    Bumped version number to 0.9.2

--- src/netxs/apps/calc.cpp
+++ src/netxs/apps/calc.cpp
@@ -1,7 +1,7 @@
 // Copyright (c) NetXS Group.
 // Licensed under the MIT license.
 
-#define DESKTOPIO_VER "v0.9.1"
+#define DESKTOPIO_VER "v0.9.2"
 #define DESKTOPIO_MYNAME "Desktopio Calc " DESKTOPIO_VER
 #define DESKTOPIO_MYPATH "vtm/calc"
 #define DESKTOPIO_DEFAPP "Calc"
