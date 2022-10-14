a374e04a5663b4451d6efdf4c2941ecc09add4a2
Dmitry Sapozhnikov <11535558+o-sdn-o@users.noreply.github.com>
2022-10-14 15:51:46 +0500

    Bumped version number to 0.9.3

--- src/netxs/apps/term.cpp
+++ src/netxs/apps/term.cpp
@@ -1,7 +1,7 @@
 // Copyright (c) NetXS Group.
 // Licensed under the MIT license.
 
-#define DESKTOPIO_VER "v0.9.2"
+#define DESKTOPIO_VER "v0.9.3"
 #define DESKTOPIO_MYNAME "Desktopio Terminal " DESKTOPIO_VER
 #define DESKTOPIO_MYPATH "vtm/term"
 #define DESKTOPIO_DEFAPP "Term"
