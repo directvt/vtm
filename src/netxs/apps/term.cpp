43672aa70782bcb376d29c300b2b90d6094709fb
Dmitry Sapozhnikov <11535558+o-sdn-o@users.noreply.github.com>
2022-12-16 21:26:03 +0500

    v0.9.8b

--- src/netxs/apps/term.cpp
+++ src/netxs/apps/term.cpp
@@ -1,7 +1,7 @@
 // Copyright (c) NetXS Group.
 // Licensed under the MIT license.
 
-#define DESKTOPIO_VER "v0.9.8a"
+#define DESKTOPIO_VER "v0.9.8b"
 #define DESKTOPIO_MYNAME "Desktopio Terminal " DESKTOPIO_VER
 #define DESKTOPIO_MYPATH "vtm/term"
 #define DESKTOPIO_DEFAPP "Term"
