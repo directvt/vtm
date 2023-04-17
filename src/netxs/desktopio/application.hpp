f748d46126fbbefda51d9946408174811f13cfee
Dmitry Sapozhnikov <11535558+o-sdn-o@users.noreply.github.com>
2023-04-17 14:58:38 +0500

    v0.9.9

--- src/netxs/desktopio/application.hpp
+++ src/netxs/desktopio/application.hpp
@@ -21,7 +21,7 @@ namespace netxs::app
 
 namespace netxs::app::shared
 {
-    static const auto version = "v0.9.8w";
+    static const auto version = "v0.9.9";
     static const auto desktopio = "desktopio";
     static const auto logsuffix = "_log";
     static const auto usr_config = "~/.config/vtm/settings.xml";
