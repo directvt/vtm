196dff2a916ada317aec3963f2fa51ca1fb42d0e
Dmitry Sapozhnikov <11535558+o-sdn-o@users.noreply.github.com>
2023-05-06 21:39:39 +0500

    Slightly more intelligent resizing the scrollback buffer under Windows

--- src/netxs/desktopio/consrv.hpp
+++ src/netxs/desktopio/consrv.hpp
@@ -1271,6 +1271,27 @@ struct consrv
         if (!result) { log("\tinvalid handle 0x", utf::to_hex(handle_ptr)); }
         return result;
     }
+    void check_buffer_size(auto& console, auto& size)
+    {
+        if (size.x > 165)
+        {
+            // Just disable wrapping if user application requests too much (Explicit requirement for horizontal scrolling).
+            // E.g. wmic requests { x=1500, y=300 }.
+            //      Indep stat for dwSize.X = N: max: 1280, 10000, 192, 237, 200, 2500, 500, 600, 640.
+            //                                   min: 150, 100, 132, 120, 150, 140, 165, 30, 80.
+            log("\ttoo wide buffer requested, turning off wrapping");
+            console.style.wrp(faux);
+            size.x = console.panel.x;
+        }
+        if (size.y > 99)
+        {
+             // Applications usually request real viewport heights: 20, 24, 25, 50
+             //         or extra large values for the scrollbuffer: 0x7FFF, 5555, 9000, 9999, 4096, 32767, 32000, 10000, 2500, 2000, 1024, 999, 800, 512, 500, 480, 400, 300, 100 etc. (stat for dwSize.Y = N)
+            log("\ttoo long buffer requested, updating scrollback limits");
+            uiterm.sb_min(size.y);
+            size.y = console.panel.y;
+        }
+    }
 
     template<class S, class P>
     auto write_block(S& scrollback, core const& block, twod coor, rect clip, P fuse)
@@ -2559,8 +2580,15 @@ struct consrv
         auto windowsz = twod{ packet.input.windowsz_x, packet.input.windowsz_y };
         console.cup0(caretpos);
         console.brush.meta(attr_to_brush(packet.input.attributes));
-        if (&console != uiterm.target) console.resize_viewport(buffsize);
-        else                           uiterm.window_resize(windowsz);
+        if (&console != uiterm.target) // If not active buffer.
+        {
+            check_buffer_size(console, buffsize);
+            console.resize_viewport(buffsize);
+        }
+        else // If active buffer.
+        {
+            uiterm.window_resize(windowsz);
+        }
         log("\tbuffer size ", buffsize);
         log("\tcursor coor ", twod{ packet.input.cursorposx, packet.input.cursorposy });
         log("\twindow coor ", twod{ packet.input.windowposx, packet.input.windowposy });
@@ -2602,24 +2630,13 @@ struct consrv
         auto size = twod{ packet.input.buffersz_x, packet.input.buffersz_y };
         log("\tinput.size ", size);
 
-        if (size.x > 165) // E.g. wmic requests { 1500, 300 }. max: 1280, 10000, 192, 237, 200, 2500, 500, 600, 640.
-        {                                                   // min: 150, 100, 132, 120, 150, 140, 165, 30, 80.
-            log("\ttoo wide buffer requested, turning off wrapping");
-            console.style.wrp(faux);
-            size.x = console.panel.x;
-        }
-        if (size.y > 99) // Apps usually request either real viewport height (20,24,25,50) or extra high: 0x7FFF, 5555, 9000, 9999, 4096, 32767, 32000, 10000, 2500, 2000, 1024, 999, 800, 512, 500, 480, 400, 300, 100 etc. (dwSize.Y = N)
-        {
-            log("\ttoo long buffer requested, updating scrollback limits");
-            uiterm.sb_min(size.y);
-            size.y = console.panel.y;
-        }
-        if (packet.target->link != &uiterm.target)
+        if (packet.target->link != &uiterm.target) // It is additional/alternate buffer.
         {
             console.resize_viewport(size);
         }
         else
         {
+            check_buffer_size(console, size);
             uiterm.window_resize(size);
         }
 
