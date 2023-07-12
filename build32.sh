317f1c125831e21a47795b053b6918ba66ef774f
Dmitry Sapozhnikov <11535558+o-sdn-o@users.noreply.github.com>
2023-07-12 14:32:56 +0500

    #423 Fix build32.sh

--- build32.sh
+++ build32.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
 # Make sure you have additional cross-compilation libraries installed.
-#   E.g.: sudo apt install g++-multilib
+#   E.g.: sudo apt install gcc-i686-linux-gnu g++-i686-linux-gnu
+#         sudo apt install gcc-arm-linux-gnueabihf g++-arm-linux-gnueabihf
+
 ./cleanup.sh
-cmake ./src -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS="-static -pthread -s -m32"
-cmake --build .
+
+# Compile the x86 target binary:
+#cmake ./src -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER="/bin/i686-linux-gnu-g++" -DCMAKE_CXX_FLAGS="-static -pthread -s" && cmake --build .
+
+# Compile the ARM32 target binary
+cmake ./src -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER="/bin/arm-linux-gnueabihf-g++" -DCMAKE_CXX_FLAGS="-static -pthread -s" && cmake --build .
