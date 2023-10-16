48035bdd1a4ac515c17e23d01498ec889a3eea37
Dmitry Sapozhnikov <11535558+o-sdn-o@users.noreply.github.com>
2023-10-16 23:54:51 +0500

    #396 Implement word wrapping in window titles and in dtvt-apps

--- src/netxs/desktopio/richtext.hpp
+++ src/netxs/desktopio/richtext.hpp
@@ -125,12 +125,28 @@ namespace netxs::ui
                 outwidth = printout.coor.x + printout.size.x - textline.coor.x;
 
                 if constexpr (!Split)
-                if (printout.size.x > 1)
+                if (printout.size.x > 1 && textline.size.x > printout.size.x)
                 {
-                    auto p = curpoint + printout.size.x - 1;
-                    if (block.at(p).wdt() == 2)
+                    auto n = printout.size.x - 1;
+                    auto p = curpoint + n;
+                    while (n)
                     {
-                        --printout.size.x;
+                        auto& c = block.at(p);
+                        if (c.isspc() || c.wdt() == 3) break;
+                        n--;
+                        p--;
+                    }
+                    if (n > 0) // Cut by whitespace.
+                    {
+                        printout.size.x = n + 1;
+                    }
+                    else // Cut on a widechar boundary (CJK/Emoji).
+                    {
+                        auto p = curpoint + printout.size.x - 1;
+                        if (block.at(p).wdt() == 2)
+                        {
+                            --printout.size.x;
+                        }
                     }
                 }
             }
