31bde3902a137424441e3f97bf4792c35ce83ae0
Dmitry Sapozhnikov <11535558+o-sdn-o@users.noreply.github.com>
2023-10-17 16:07:10 +0500

    Workaround for WinSrv2019 which randomly loses the reverse video SGR attribute

--- src/netxs/desktopio/directvt.hpp
+++ src/netxs/desktopio/directvt.hpp
@@ -1108,6 +1108,7 @@ namespace netxs::directvt
                         right_half(next);
                     }
                 };
+                block.basevt::inv(state.inv()); //todo Windows Server 2019 loses the reverse video SGR attribute.
                 if (image.hash() != cache.hash())
                 {
                     block.basevt::scroll_wipe();
