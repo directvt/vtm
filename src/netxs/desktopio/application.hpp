c55c517b09bc9023416a74039a46d71ad673c29a
Dmitry Sapozhnikov <11535558+o-sdn-o@users.noreply.github.com>
2024-01-17 17:19:28 +0500

    v0.9.55

--- src/netxs/desktopio/application.hpp
+++ src/netxs/desktopio/application.hpp
@@ -23,7 +23,7 @@ namespace netxs::app
 
 namespace netxs::app::shared
 {
-    static const auto version = "v0.9.54";
+    static const auto version = "v0.9.55";
     static const auto repository = "https://github.com/directvt/vtm";
     static const auto ipc_prefix = "vtm";
     static const auto log_suffix = "_log";
