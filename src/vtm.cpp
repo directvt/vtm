dcee6bbbf801217928750d32f0de003614ca0cac
Dmitry Sapozhnikov <11535558+o-sdn-o@users.noreply.github.com>
2024-01-29 01:49:46 +0500

    Update notes (logo)

--- src/vtm.cpp
+++ src/vtm.cpp
@@ -101,9 +101,8 @@ int main(int argc, char* argv[])
             os::dtvt::initialize();
             netxs::logger::wipe();
             auto syslog = os::tty::logger();
-            log("\nText-based desktop environment " + text{ app::shared::version } +
-                "\nhttps://github.com/directvt/vtm"
-                "\n"
+            log("\nText-based ██   https://github.com/directvt/vtm"
+                "\n             ██ Desktop Environment " + text{ app::shared::version } +
                 "\n  Syntax:"
                 "\n"
                 "\n    " + os::process::binary<true>() + " [ -i | -u ] | [ -v ] | [ -? ]"
