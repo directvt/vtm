8b453a1e4984482c9f5494df4a68c6afcc631bd1
Dmitry Sapozhnikov <11535558+o-sdn-o@users.noreply.github.com>
2024-10-25 01:25:51 +0500

    Fix apple clang complaints

--- src/netxs/desktopio/controls.hpp
+++ src/netxs/desktopio/controls.hpp
@@ -1159,7 +1159,7 @@ namespace netxs::ui
                     {
                         auto backup = boss.This();
                         log(prompt::gate, "No mouse clicking events");
-                        boss.template base::riseup<tier::release>(e2::form::proceed::quit::one, true);
+                        boss.base::template riseup<tier::release>(e2::form::proceed::quit::one, true);
                         ping.reset();
                         memo.clear();
                     }
