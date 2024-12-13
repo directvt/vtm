649a972990fef7e5d78d01e01c85d5610d8cf0b4
Dmitry Sapozhnikov <11535558+o-sdn-o@users.noreply.github.com>
2024-12-13 22:39:15 +0500

    Minor edits

--- src/netxs/desktopio/console.hpp
+++ src/netxs/desktopio/console.hpp
@@ -1112,7 +1112,7 @@ namespace netxs::ui
         }
 
         //todo revise
-        gate(xipc uplink, si32 vtmode, xmls& config, view userid = {}, si32 session_id = 0, bool isvtm = faux, bool is_recursive = faux)
+        gate(xipc uplink, si32 vtmode, xmls& config, view userid = {}, si32 session_id = 0, bool isvtm = faux, bool recursive = faux)
             : canal{ *uplink },
               props{ canal, userid, vtmode, isvtm, session_id, config },
               input{ props, *this },
@@ -1125,7 +1125,7 @@ namespace netxs::ui
               local{ true },
               yield{ faux },
               fullscreen{ faux },
-              is_recursive{ is_recursive }
+              is_recursive{ recursive }
         {
             keybd.proc("ToggleDebugOverlay", [&](hids& gear, txts&){ gear.set_handled(); debug ? debug.stop() : debug.start(); });
             auto bindings = pro::keybd::load(config, "tui");
