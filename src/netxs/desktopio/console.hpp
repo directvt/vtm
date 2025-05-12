61b9bbe2efd06f61e82b4f77b1fa5fa17bf72b19
Dmitry Sapozhnikov <11535558+o-sdn-o@users.noreply.github.com>
2025-05-12 22:43:26 +0500

    Fix recursive connection

--- src/netxs/desktopio/console.hpp
+++ src/netxs/desktopio/console.hpp
@@ -739,7 +739,7 @@ namespace netxs::ui
         void launch()
         {
             auto root_ptr = This();
-            base::broadcast(tier::anycast, e2::form::upon::started, root_ptr); // Make all stuff ready to receive input.
+            base::signal(tier::anycast, e2::form::upon::started, root_ptr); // Make all stuff ready to receive input.
             directvt::binary::stream::reading_loop(canal, [&](view data){ conio.s11n::sync(data); });
             conio.s11n::stop(); // Wake up waiting dtvt objects, if any.
             if constexpr (debugmode) log(prompt::gate, "DirectVT session closed");
