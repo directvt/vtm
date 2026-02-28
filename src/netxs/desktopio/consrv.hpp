87139c29df3961ea7e05ba4812c44cf128e67f76
Dmitry Sapozhnikov <11535558+o-sdn-o@users.noreply.github.com>
2026-02-28 22:25:07 +0500

    Store index colors (0-255) in the built-in terminal as is

--- src/netxs/desktopio/consrv.hpp
+++ src/netxs/desktopio/consrv.hpp
@@ -2222,30 +2222,21 @@ struct impl : consrv
     }
     auto attr_to_brush(ui16 attr)
     {
-        auto& colors = uiterm.ctrack.color;
         auto c = cell{ whitespace }
-            .fgc(colors[netxs::swap_bits<0, 2>(attr      & 0x000Fu)]) // FOREGROUND_ . . .
-            .bgc(colors[netxs::swap_bits<0, 2>(attr >> 4 & 0x000Fu)]) // BACKGROUND_ . . .
             .inv(!!(attr & COMMON_LVB_REVERSE_VIDEO  ))
             .und(!!(attr & COMMON_LVB_UNDERSCORE     ))
             .ovr(!!(attr & COMMON_LVB_GRID_HORIZONTAL));
+        argb::set_indexed_color(c.fgc(), netxs::swap_bits<0, 2>(attr      & 0x000Fu)); // FOREGROUND_ . . .
+        argb::set_indexed_color(c.bgc(), netxs::swap_bits<0, 2>(attr >> 4 & 0x000Fu)); // BACKGROUND_ . . .
         return c;
     }
     auto brush_to_attr(cell const& brush)
     {
         auto attr = ui16{};
-        auto fgcx = 7_sz; // Fallback for true colors.
-        auto bgcx = 0_sz;
-        auto frgb = brush.fgc().token;
-        auto brgb = brush.bgc().token;
-        auto head = std::begin(uiterm.ctrack.color);
-        for (auto i = 0; i < 16; i++)
-        {
-            auto const& c = *head++;
-            auto m = netxs::swap_bits<0, 2>(i); // ANSI<->DOS color scheme reindex.
-            if (c == frgb) fgcx = m;
-            if (c == brgb) bgcx = m;
-        }
+        auto fgcx = argb::is_indexed_color(brush.fgc());
+        auto bgcx = argb::is_indexed_color(brush.bgc());
+        if (fgcx) fgcx = netxs::swap_bits<0, 2>(fgcx - 1); else fgcx = 7; // Fallback to BW for non-index colors.
+        if (bgcx) bgcx = netxs::swap_bits<0, 2>(bgcx - 1); else bgcx = 0; //
         attr = (ui16)(fgcx + (bgcx << 4));
         if (brush.inv()) attr |= COMMON_LVB_REVERSE_VIDEO;
         if (brush.und()) attr |= COMMON_LVB_UNDERSCORE;
@@ -3989,22 +3980,30 @@ struct impl : consrv
         auto bgcx = 0_sz;
         auto& rgbpalette = packet.reply.rgbpalette;
         auto mark = console.get_effective_brush();
-        auto frgb = mark.fgc().token;
-        auto brgb = mark.bgc().token;
+        auto fg = mark.fgc();
+        auto bg = mark.bgc();
+        if (auto i = argb::is_indexed_color(fg))
+        {
+            fgcx = netxs::swap_bits<0, 2>(i - 1);
+        }
+        else if (fg.token)
+        {
+            fgcx = fg.to_vga16(true);
+        }
+        if (auto i = argb::is_indexed_color(bg))
+        {
+            bgcx = netxs::swap_bits<0, 2>(i - 1);
+        }
+        else if (bg.token)
+        {
+            bgcx = bg.to_vga16(faux);
+        }
         auto head = std::begin(uiterm.ctrack.color);
         for (auto i = 0; i < 16; i++)
         {
             auto const& c = *head++;
             auto m = netxs::swap_bits<0, 2>(i); // ANSI<->DOS color scheme reindex.
             rgbpalette[m] = argb::swap_rb(c); // conhost crashes if alpha non zero.
-            if (c == frgb) fgcx = m;
-            if (c == brgb) bgcx = m + 1;
-        }
-        if (brgb && !bgcx--) // Reset background if true colors are used.
-        {
-            bgcx = 0;
-            uiterm.ctrack.color[bgcx] = brgb;
-            rgbpalette         [bgcx] = argb::swap_rb(brgb); // conhost crashes if alpha non zero.
         }
         packet.reply.attributes = (ui16)(fgcx + (bgcx << 4));
         if (mark.inv()) packet.reply.attributes |= COMMON_LVB_REVERSE_VIDEO;
