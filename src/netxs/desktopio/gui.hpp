1b8c3571798f6cb05305d1ac9a45e55b8ec2bbc0
Dmitry Sapozhnikov <11535558+o-sdn-o@users.noreply.github.com>
2026-03-05 02:35:39 +0500

    Workaround for the 'HWND_TOPMOST win32' bug (after Win+D pressed twice)

--- src/netxs/desktopio/gui.hpp
+++ src/netxs/desktopio/gui.hpp
@@ -2514,12 +2514,14 @@ namespace netxs::gui
             }
         }
         si32 prev_window_state = winstate::normal;
+        bool ontop_state = faux;
         void restore_if_minimized() // Restore focused window.
         {
             if (fsmode == winstate::minimized) // We are restoring from an implicit minimized state.
             {
                 auto restored_state = std::exchange(prev_window_state, winstate::normal);
                 set_state(restored_state);
+                window_make_topmost(ontop_state); // Restore AlwaysOnTop state.
             }
         }
         void check_window(twod coor)
@@ -4366,7 +4368,7 @@ namespace netxs::gui
         }
         void window_make_focused()       { restore_if_minimized(); ::SetFocus((HWND)master.hWnd); } // Calls WM_KILLFOCOS(prev) + WM_ACTIVATEAPP(next) + WM_SETFOCUS(next).
         void window_make_exposed()       { ::SetWindowPos((HWND)master.hWnd, HWND_TOP, 0, 0, 0, 0, SWP_NOSIZE | SWP_NOMOVE | SWP_NOSENDCHANGING | SWP_NOACTIVATE); }
-        void window_make_topmost(bool s) { ::SetWindowPos((HWND)master.hWnd, s ? HWND_TOPMOST : HWND_NOTOPMOST, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE); }
+        void window_make_topmost(bool s) { ontop_state = s; ::SetWindowPos((HWND)master.hWnd, s ? HWND_TOPMOST : HWND_NOTOPMOST, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE); }
         void window_make_foreground()    { ::SetForegroundWindow((HWND)master.hWnd); } //::AllowSetForegroundWindow(ASFW_ANY); } // Neither ::SetFocus() nor ::SetActiveWindow() can switch focus immediately.
         void window_shutdown()           { ::SendMessageW((HWND)master.hWnd, WM_CLOSE, NULL, NULL); }
         void window_cleanup()            { ::RemoveClipboardFormatListener((HWND)master.hWnd); ::PostQuitMessage(0); }
