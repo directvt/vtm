162bd430e7196ba336f3af1faf3e48b3db8eb7fd
Dmitry Sapozhnikov <11535558+o-sdn-o@users.noreply.github.com>
2026-03-06 18:21:34 +0500

    Remove the default translucency of non-scrolling regions (top/bottom/left/right)

--- src/netxs/desktopio/terminal.hpp
+++ src/netxs/desktopio/terminal.hpp
@@ -312,7 +312,7 @@ namespace netxs::ui
                 resetonkey =             config.settings::take("/config/terminal/scrollback/reset/onkey",     true);
                 resetonout =             config.settings::take("/config/terminal/scrollback/reset/onoutput",  faux);
                 def_alt_on =             config.settings::take("/config/terminal/scrollback/altscroll",       true);
-                def_lucent = std::max(0, config.settings::take("/config/terminal/scrollback/oversize/opacity",si32{ 0xC0 } ));
+                def_lucent = std::max(0, config.settings::take("/config/terminal/scrollback/oversize/opacity",si32{ 0xFF } ) & 0xFF);
                 def_margin = std::max(0, config.settings::take("/config/terminal/scrollback/oversize",        si32{ 0 }    ));
                 def_tablen = std::max(1, config.settings::take("/config/terminal/tablen",                     si32{ 8 }    ));
                 def_border = std::max(0, config.settings::take("/config/terminal/border",                     si32{ 0 }    ));
